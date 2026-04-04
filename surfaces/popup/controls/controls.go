99a6d3a5fe762f0a486286edf471620ad9b7ab3c
Son Roy Almerol <github@snry.me>
2026-04-04 18:42:14 -0400

    Fix popup positioning: always set margins and add debug logging

--- surfaces/popup/controls/controls.go
+++ surfaces/popup/controls/controls.go
@@ -1,6 +1,9 @@
 package controls
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/diamondburned/gotk4/pkg/glib/v2"
 	"github.com/diamondburned/gotk4/pkg/gtk/v4"
 	"github.com/sonroyaalmerol/snry-shell/internal/bus"
@@ -90,22 +93,28 @@ func (c *Controls) build(refs *servicerefs.ServiceRefs) {
 }
 
 func (c *Controls) positionUnderTrigger() {
-	monW := surfaceutil.MonitorWidth()
-	if monW <= 0 {
-		return
-	}
-
 	triggerX := surfaceutil.WidgetXRelativeToRoot(c.trigger)
 	triggerW := surfaceutil.WidgetWidth(c.trigger)
 	popupW := panelWidth + panelMargin*2
+	monW := surfaceutil.MonitorWidth()
+
+	fmt.Fprintf(os.Stderr, "controls: triggerX=%d triggerW=%d popupW=%d monW=%d\n",
+		triggerX, triggerW, popupW, monW)
 
 	desiredLeft := triggerX + triggerW/2 - popupW/2
-	if desiredLeft < panelMargin {
-		desiredLeft = panelMargin
+	if monW > 0 {
+		if desiredLeft < panelMargin {
+			desiredLeft = panelMargin
+		}
+		if desiredLeft+popupW > monW-panelMargin {
+			desiredLeft = monW - panelMargin - popupW
+		}
+	} else {
+		fmt.Fprintln(os.Stderr, "controls: monW=0, skipping clamp")
 	}
-	if desiredLeft+popupW > monW-panelMargin {
-		desiredLeft = monW - panelMargin - popupW
-	}
+
+	fmt.Fprintf(os.Stderr, "controls: desiredLeft=%d marginTop=%d\n",
+		desiredLeft, layershell.BarExclusiveZone+8)
 
 	c.root.SetMarginTop(layershell.BarExclusiveZone + 8)
 	c.root.SetMarginStart(desiredLeft)
--- surfaces/popup/notifcenter/notifcenter.go
+++ surfaces/popup/notifcenter/notifcenter.go
@@ -1,6 +1,9 @@
 package notifcenter
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/diamondburned/gotk4/pkg/glib/v2"
 	"github.com/diamondburned/gotk4/pkg/gtk/v4"
 	"github.com/sonroyaalmerol/snry-shell/internal/bus"
@@ -91,21 +94,22 @@ func (nc *NotifCenter) build(refs *servicerefs.ServiceRefs) {
 }
 
 func (nc *NotifCenter) positionUnderTrigger() {
-	monW := surfaceutil.MonitorWidth()
-	if monW <= 0 {
-		return
-	}
-
 	triggerX := surfaceutil.WidgetXRelativeToRoot(nc.trigger)
 	triggerW := surfaceutil.WidgetWidth(nc.trigger)
 	popupW := panelWidth + panelMargin*2
+	monW := surfaceutil.MonitorWidth()
+
+	fmt.Fprintf(os.Stderr, "notifcenter: triggerX=%d triggerW=%d popupW=%d monW=%d\n",
+		triggerX, triggerW, popupW, monW)
 
 	desiredLeft := triggerX + triggerW/2 - popupW/2
-	if desiredLeft < panelMargin {
-		desiredLeft = panelMargin
-	}
-	if desiredLeft+popupW > monW-panelMargin {
-		desiredLeft = monW - panelMargin - popupW
+	if monW > 0 {
+		if desiredLeft < panelMargin {
+			desiredLeft = panelMargin
+		}
+		if desiredLeft+popupW > monW-panelMargin {
+			desiredLeft = monW - panelMargin - popupW
+		}
 	}
 
 	nc.root.SetMarginTop(layershell.BarExclusiveZone + 8)
