e9e3b2305e6afe03eef79b74d3ebc38aad2b6c80
Son Roy Almerol <github@snry.me>
2026-04-12 13:45:49 -0400

    Revert "bus: revert to sync.Mutex, xsync.Map caused deadlocks on re-entrant publish"
    This reverts commit 6f33cec0bddbbabb23e0cae6e9eb308a049d7e27.

--- internal/bus/bus.go
+++ internal/bus/bus.go
@@ -1,6 +1,10 @@
 package bus
 
-import "sync"
+import (
+	"sync/atomic"
+
+	"github.com/puzpuzpuz/xsync/v4"
+)
 
 type Topic string
 
@@ -60,51 +64,52 @@ type entry struct {
 }
 
 type Bus struct {
-	mu     sync.Mutex
-	subs   map[Topic][]entry
-	last   map[Topic]Event
-	nextID uint64
+	subs   *xsync.Map[Topic, []entry]
+	last   *xsync.Map[Topic, Event]
+	nextID atomic.Uint64
 }
 
 func New() *Bus {
 	return &Bus{
-		subs: make(map[Topic][]entry),
-		last: make(map[Topic]Event),
+		subs: xsync.NewMap[Topic, []entry](),
+		last: xsync.NewMap[Topic, Event](),
 	}
 }
 
 func (b *Bus) Subscribe(topic Topic, h Handler) UnsubscribeFunc {
-	b.mu.Lock()
-	id := b.nextID
-	b.nextID++
-	b.subs[topic] = append(b.subs[topic], entry{id: id, h: h})
-	ev, ok := b.last[topic]
-	b.mu.Unlock()
+	id := b.nextID.Add(1)
 
-	if ok {
+	b.subs.Compute(topic, func(old []entry, loaded bool) ([]entry, xsync.ComputeOp) {
+		return append(old, entry{id: id, h: h}), xsync.UpdateOp
+	})
+
+	if ev, ok := b.last.Load(topic); ok {
 		h(ev)
 	}
 
 	return func() {
-		b.mu.Lock()
-		defer b.mu.Unlock()
-		entries := b.subs[topic]
-		for i, e := range entries {
-			if e.id == id {
-				b.subs[topic] = append(entries[:i], entries[i+1:]...)
-				return
+		b.subs.Compute(topic, func(old []entry, loaded bool) ([]entry, xsync.ComputeOp) {
+			if !loaded {
+				return nil, xsync.DeleteOp
 			}
-		}
+			for i, e := range old {
+				if e.id == id {
+					return append(old[:i:i], old[i+1:]...), xsync.UpdateOp
+				}
+			}
+			return old, xsync.CancelOp
+		})
 	}
 }
 
 func (b *Bus) Publish(topic Topic, data any) {
-	b.mu.Lock()
 	ev := Event{Topic: topic, Data: data}
-	b.last[topic] = ev
-	entries := make([]entry, len(b.subs[topic]))
-	copy(entries, b.subs[topic])
-	b.mu.Unlock()
+	b.last.Store(topic, ev)
+
+	entries, ok := b.subs.Load(topic)
+	if !ok {
+		return
+	}
 
 	for _, e := range entries {
 		e.h(ev)
